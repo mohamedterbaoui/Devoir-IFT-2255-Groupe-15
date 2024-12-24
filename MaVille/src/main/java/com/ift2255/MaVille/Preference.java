5806990dd1cf0659c990f7621d41d0bf7be2df21
Mohamed Terbaoui <m.terbaoui7@gmail.com>
2024-12-23 21:40:00 -0500

    Ajout du code modifier de Thomas pour la classe Preference

--- MaVille/src/main/java/com/ift2255/MaVille/Preference.java
+++ MaVille/src/main/java/com/ift2255/MaVille/Preference.java
@@ -7,36 +7,43 @@ package com.ift2255.MaVille;
 public class Preference {
     int preferenceId;
     String schedule;
-  /**Constructeur pour les préférences
-   * @param id L'id de préférence
-   * @param schedule L'horaire pour les travaux
-   */  
+    /**Constructeur pour les préférences
+     * @param id L'id de préférence
+     * @param schedule L'horaire pour les travaux
+     */
     public Preference(int id, String schedule){
         this.preferenceId = id;
         this.schedule = schedule;
     }
-/** Afficher l'id de la préférence
- * @return l'ID de la préférence
- */
+
+    public Preference(String schedule){
+        this.schedule = schedule;
+    }
+    /** Afficher l'id de la préférence
+     * @return l'ID de la préférence
+     */
     public int getPreferenceId() {
         return preferenceId;
     }
-/**Afficher l'horaire de la préférence
- * @return L'horaire préfére
- */
+    /**Afficher l'horaire de la préférence
+     * @return L'horaire préfére
+     */
     public String getSchedule() {
         return schedule;
     }
-/**Changer l'ID pour le préférence
- * @param preferenceId le nouveau préférence id
- */
+    /**Changer l'ID pour le préférence
+     * @param preferenceId le nouveau préférence id
+     */
     public void setPreferenceId(int preferenceId) {
         this.preferenceId = preferenceId;
     }
-/**changer l'horaire de ce préférence
- * @param schedule la nouvelle préfèrence
- */
+    /**changer l'horaire de ce préférence
+     * @param schedule la nouvelle préfèrence
+     */
     public void setSchedule(String schedule) {
         this.schedule = schedule;
+    }
+    public String toString(){
+        return this.schedule;
     }
 }
